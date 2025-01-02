980d6f65c0e7650da058a5c36f761e67f9f4b271
zaid12z <129253512+zaid12z@users.noreply.github.com>
2025-01-03 02:24:55 +0300

    Delete public directory

--- public/js/auth/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Get environment variables from meta tags
-const supabaseUrl = document.querySelector('meta[name="supabase-url"]').content;
-const supabaseKey = document.querySelector('meta[name="supabase-key"]').content;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
